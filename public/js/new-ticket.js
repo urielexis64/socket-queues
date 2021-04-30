5aabc5a4646560e74ce34715edc23eaf52bbab6c
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-30 17:50:16 -0600

    new ticket socket

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,24 @@
+// HTML references
 
+const lblNewTicket = document.querySelector("#lblNewTicket");
+const btnCreate = document.querySelector("button");
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+socket.on("connect", () => {
+	btnCreate.disabled = false;
+});
+
+socket.on("disconnect", () => {
+	btnCreate.disabled = true;
+});
+
+socket.on("last-ticket", (ticket) => {
+	this.lblNewTicket.innerText = ticket;
+});
+
+btnCreate.addEventListener("click", () => {
+	socket.emit("next-ticket", null, (ticket) => {
+		this.lblNewTicket.innerText = ticket;
+	});
+});
