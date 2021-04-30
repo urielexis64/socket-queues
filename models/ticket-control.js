77216b256a7221dc2062627735dbdfe2a100e3c2
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-30 11:08:00 -0600

    Ticket class, next & attendTicket methods

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const fs = require("fs");
 
+class Ticket {
+	constructor(number, desktop) {
+		this.number = number;
+		this.desktop = desktop;
+	}
+}
+
 class TicketControl {
 	constructor() {
 		this.last = 0;
 		this.today = new Date().getDate();
-		this.tickers = [];
+		this.tickets = [];
 		this.last4 = [];
 
 		this.init();
@@ -15,16 +22,16 @@ class TicketControl {
 		return {
 			last: this.last,
 			today: this.today,
-			tickers: this.tickers,
+			tickets: this.tickets,
 			last4: this.last4,
 		};
 	}
 
 	init() {
-		const {today, last, tickers, last4} = require("../db/data.json");
+		const {today, last, tickets, last4} = require("../db/data.json");
 		if (today === this.today) {
 			this.last = last;
-			this.tickers = tickers;
+			this.tickets = tickets;
 			this.last4 = last4;
 		} else {
 			// Other day
@@ -35,6 +42,34 @@ class TicketControl {
 	saveDB() {
 		const dbPath = path.join(__dirname, "../db/data.json");
 		fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+	}
+
+	next() {
+		this.last += 1;
+		const ticket = new Ticket(this.last, null);
+		this.tickets.push(ticket);
+
+		this.saveDB();
+		return "Ticket " + ticket.number;
+	}
+
+	attendTicket(desktop) {
+		if (this.tickets.length === 0) {
+			return null;
+		}
+
+		const ticket = this.tickets.shift();
+		ticket.desktop = desktop;
+
+		this.last4.unshift(ticket);
+
+		if (this.last4.length > 4) {
+			this.last4.splice(-1, 1); // starts from the last element (-1)
+		}
+
+		this.saveDB();
+
+		return ticket;
 	}
 }
 
